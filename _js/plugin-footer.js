5cea1d88c161ebdea2b11f128ef054565e8425ed
Phil <philip.schaefer0@gmail.com>
2024-04-18 12:18:26 +0200

    changed branding and switched to doneEach

--- _js/plugin-footer.js
+++ _js/plugin-footer.js
@@ -1,16 +1,18 @@
 // Edit on Github https://github.com/docsifyjs/docsify/blob/167596be51d8918efa2238a0e12eab14445e4022/docs/index.html#L193C16-L193C16
 // footer https://github.com/boopathikumar018/docsify-darklight-theme/blob/master/docs/index.html        
-function footer(hook, vm) {
+function footerPlugin(hook, vm) {
   const footer = [
     '<footer style="text-align: center; padding-top: 40px;">',
-    'Unoffical Resonite Wiki &copy; 2024',
+    'Karlo-Hosting &copy; 2024',
     "<br/>",
-    'Made with ❤ by <a href="https://github.com/Karlo-Hosting" target="_blank">@Karlo-Hosting</a> and <a href="https://github.com/Karlo-Hosting/Docs/graphs/contributors" target="_blank">Helpers</a>',
+    'Made with <span style="color: lightcoral">❤</span> by <a href="https://github.com/Karlo-Hosting" target="_blank">@Karlo-Hosting</a> and <a href="https://github.com/Karlo-Hosting/Docs/graphs/contributors" target="_blank">Helpers</a>',
     "</footer>",
   ].join("");
 
-  // TODO: Look into what this does
-  hook.beforeEach((html) => {
+  hook.doneEach(() => {
+    const html = document.querySelector('main section article');
+    
+    // TODO: Look into what this does
     if (/githubusercontent\.com/.test(vm.route.file)) {
       url = vm.route.file
         .replace("raw.githubusercontent.com", "github.com")
@@ -24,11 +26,12 @@ function footer(hook, vm) {
         "https://github.com/Karlo-Hosting/Docs/blob/main/" +
         vm.route.file;
     }
-    const editText = `\n<div style="text-align: right"><a href="${url}" target="_blank">:memo: Edit on GitHub</a></div>`;
+    const editText = `\n<div style="text-align: right"><a href="${url}" target="_blank">📝 Edit on GitHub</a></div>`;
 
-    return html + editText + footer;
+    const parsed = new DOMParser().parseFromString([editText, footer].join(''), "text/html");
+    html.append(parsed.body)
   });
 }
 // Add plugin to docsify's plugin array
 window.$docsify = window.$docsify || {};
-$docsify.plugins = [footer, ...($docsify.plugins || [])];
+$docsify.plugins = [footerPlugin, ...($docsify.plugins || [])];
