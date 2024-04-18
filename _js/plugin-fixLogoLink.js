4a7142f0f448b040abc1745228d7d30f5d81d16e
Flipper <philip.schaefer@gmail.com>
2024-04-18 19:09:26 +0200

    add better logo routing

---
+++ _js/plugin-fixLogoLink.js
@@ -0,0 +1,10 @@
+// Edit on Github https://github.com/docsifyjs/docsify/blob/167596be51d8918efa2238a0e12eab14445e4022/docs/index.html#L193C16-L193C16
+// footer https://github.com/boopathikumar018/docsify-darklight-theme/blob/master/docs/index.html        
+function logoLinkPlugin(hook, vm) {
+  hook.doneEach(() => {
+    document.querySelector('a.app-name-link').href='/#/';
+  });
+}
+// Add plugin to docsify's plugin array
+window.$docsify = window.$docsify || {};
+$docsify.plugins = [logoLinkPlugin, ...($docsify.plugins || [])];
