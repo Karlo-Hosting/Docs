ee305b3a15f9ce3de7f01d617b76d93adca1808f
Flipper <philip.schaefer@gmail.com>
2024-04-19 00:37:13 +0200

    fix stacking endgecase

--- _js/plugin-fixLogoLink.js
+++ _js/plugin-fixLogoLink.js
@@ -1,9 +1,8 @@
-// Edit on Github https://github.com/docsifyjs/docsify/blob/167596be51d8918efa2238a0e12eab14445e4022/docs/index.html#L193C16-L193C16
-// footer https://github.com/boopathikumar018/docsify-darklight-theme/blob/master/docs/index.html        
 function logoLinkPlugin(hook, vm) {
   hook.doneEach(() => {
     const element = document.querySelector('a.app-name-link');
-    element.href = `${element.href}#/`;
+    if (element.href.endsWith('/#/')) return;
+    return element.href = `${element.href}#/`;
   });
 }
 // Add plugin to docsify's plugin array
